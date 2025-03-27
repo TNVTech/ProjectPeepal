0b6d75a6dcecfe1c363a40a3221ce08ccc9f9cdb
TNVTech <developers.tnvtech@gmail.com>
2025-03-27 14:02:24 +0000

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -101,7 +101,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`SSO Provider: ${process.env.SSO_PROVIDER}`);
